cb755b3ffb4c3a53da420d2d28fe7117909a025a
Anna <a.sobolenko1194@gmail.com>
2020-05-03 01:34:45 +0300

    feat: task comleted

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,12 +51,8 @@ function getCicleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(value1, value2) {
-  if (value1 === Infinity || value2 === Infinity) {
-    return 'Infinity';
-  }
-  const num = (value1 + value2) / 2;
-  return num;
+function getAverage(/* value1, value2 */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -208,13 +204,8 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  for (let i = 2; i < n; i + 1) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-  return n > 1;
+function isPrime(/* n */) {
+  throw new Error('Not implemented');
 }
 
 /**
