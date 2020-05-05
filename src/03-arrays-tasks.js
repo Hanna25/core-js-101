7e6f9fd8270fa1ce3daf5852746abe962aebaeb3
Anna <a.sobolenko1194@gmail.com>
2020-05-05 17:56:36 +0300

    feat: task comleted

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -307,8 +307,15 @@ function getMovingSum(/* arr */) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  const secondItem = [];
+  arr.map((currentValue, index) => {
+    if (index % 2 !== 0) {
+      secondItem.push(currentValue);
+    }
+    return '';
+  });
+  return secondItem;
 }
 
 
@@ -344,8 +351,14 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  let num;
+  if (arr.length > 3) {
+    num = arr.slice(arr.length - 3);
+  } else {
+    return arr.reverse();
+  }
+  return num.reverse();
 }
 
 
@@ -362,8 +375,15 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  const positiveNumArr = [];
+  arr.map((currentValue) => {
+    if (currentValue > 0 && typeof currentValue !== 'string') {
+      positiveNumArr.push(currentValue);
+    }
+    return 0;
+  });
+  return positiveNumArr.length;
 }
 
 /**
@@ -395,8 +415,13 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  let sum = 0;
+  arr.map((currentValue) => {
+    sum += currentValue;
+    return '';
+  });
+  return sum;
 }
 
 /**
@@ -411,8 +436,15 @@ function getItemsSum(/* arr */) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const falsy = [];
+  arr.map((currentValue) => {
+    if (!currentValue) {
+      falsy.push(currentValue);
+    }
+    return '';
+  });
+  return falsy.length;
 }
 
 /**
@@ -451,8 +483,8 @@ function findAllOccurences(arr, item) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return `${arr}`;
 }
 
 
