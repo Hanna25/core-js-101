264c84ff02f521ce33eebaeec38503faab32ccb3
Anna <a.sobolenko1194@gmail.com>
2020-09-14 17:51:37 +0300

    task completed

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,8 +51,16 @@ function getCicleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  const sumArguments = 2;
+  const sum = value1 / sumArguments + value2 / sumArguments;
+  if (sum === Number.POSITIVE_INFINITY) {
+    return Number.POSITIVE_INFINITY;
+  }
+  if (sum === Number.NEGATIVE_INFINITY) {
+    return Number.NEGATIVE_INFINITY;
+  }
+  return sum;
 }
 
 /**
@@ -204,8 +212,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
